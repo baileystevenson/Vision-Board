931fae046c91fea1a89fcd1064bb3fb2175da8dd
Amber Stevenson <amber.stevenson14@gmail.com>
2022-12-23 17:25:11 -0700

    first commit

--- public/capstone_main.js
+++ public/capstone_main.js
@@ -41,6 +41,7 @@ function addWant(text) {
   buttons.classList.add('buttons')
   let remove = document.createElement('button')
   remove.classList.add('remove')
+  remove.textContent = "Remove"
   remove.addEventListener('click', removeItem)
   buttons.appendChild(remove)
   item.appendChild(buttons)
@@ -82,8 +83,9 @@ function addInspired(text) {
   item1.innerText = text
   let buttons = document.createElement('div')
   buttons.classList.add('buttons')
-  let remove = document.createElement('button' value="Remove") 
+  let remove = document.createElement('button') 
   remove.classList.add('remove')
+  remove.textContent = "Remove"
   remove.addEventListener('click', removeItem)
   buttons.appendChild(remove)
   item1.appendChild(buttons)
@@ -126,6 +128,7 @@ function addGoal(text) {
   buttons.classList.add('buttons')
   let remove = document.createElement('button')
   remove.classList.add('remove')
+  remove.textContent = "Remove"
   remove.addEventListener('click', removeItem)
   buttons.appendChild(remove)
   item2.appendChild(buttons)
@@ -168,6 +171,7 @@ function addDream(text) {
   buttons.classList.add('buttons')
   let remove = document.createElement('button')
   remove.classList.add('remove')
+  remove.textContent = "Remove"
   remove.addEventListener('click', removeItem)
   buttons.appendChild(remove)
   item3.appendChild(buttons)
@@ -198,3 +202,165 @@ window.onclick = function(event) {
     }
   }
 }
+let puppyImg= document.getElementById('puppy')
+let picSelect = (evt) => {
+    let item1 =document.getElementById('item1')
+    item1.style.backgroundImage = `url(${evt.target.src})`
+}
+
+puppyImg.addEventListener("click",picSelect)
+
+let beachImg= document.getElementById('beach')
+let picSelect1 = (evt) => {
+    let item1 =document.getElementById('item1')
+    item1.style.backgroundImage = `url(${evt.target.src})`
+}
+
+beachImg.addEventListener("click",picSelect)
+
+
+
+let careerImg= document.getElementById('career')
+let picSelect2 = (evt) => {
+    let item1 =document.getElementById('item1')
+    item1.style.backgroundImage = `url(${evt.target.src})`
+}
+
+careerImg.addEventListener("click",picSelect)
+
+
+let travelImg= document.getElementById('travel')
+let picSelect3 = (evt) => {
+    let item1 =document.getElementById('item1')
+    item1.style.backgroundImage = `url(${evt.target.src})`
+}
+
+travelImg.addEventListener("click",picSelect)
+
+
+let houseImg= document.getElementById('house')
+let picSelect4 = (evt) => {
+    let item1 =document.getElementById('item1')
+    item1.style.backgroundImage = `url(${evt.target.src})`
+}
+
+houseImg.addEventListener("click",picSelect)
+
+
+
+let familyImg= document.getElementById('family')
+let picSelect5 = (evt) => {
+    let item1 =document.getElementById('item1')
+    item1.style.backgroundImage = `url(${evt.target.src})`
+}
+
+familyImg.addEventListener("click",picSelect)
+
+
+let relaxImg= document.getElementById('relax')
+let picSelect6 = (evt) => {
+    let item1 =document.getElementById('item1')
+    item1.style.backgroundImage = `url(${evt.target.src})`
+}
+
+relaxImg.addEventListener("click",picSelect)
+
+let carImg= document.getElementById('car')
+let picSelect7 = (evt) => {
+    let item1 =document.getElementById('item1')
+    item1.style.backgroundImage = `url(${evt.target.src})`
+}
+
+carImg.addEventListener("click",picSelect)
+
+
+///Drop Down 2///
+
+function myFunction2() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+let puppyImg1= document.getElementById('puppy1')
+let picSelect8 = (evt) => {
+    let item2 =document.getElementById('item2')
+    item2.style.backgroundImage = `url(${evt.target.src})`
+}
+
+puppyImg1.addEventListener("click",picSelect)
+
+let beachImg1= document.getElementById('beach')
+let picSelect9 = (evt) => {
+    let item2 =document.getElementById('item2')
+    item2.style.backgroundImage = `url(${evt.target.src})`
+}
+
+beachImg.addEventListener("click",picSelect)
+
+
+
+let careerImg1= document.getElementById('career')
+let picSelect10 = (evt) => {
+    let item2 =document.getElementById('item2')
+    item2.style.backgroundImage = `url(${evt.target.src})`
+}
+
+careerImg.addEventListener("click",picSelect)
+
+
+let travelImg1= document.getElementById('travel')
+let picSelect11 = (evt) => {
+    let item2 =document.getElementById('item2')
+    item2.style.backgroundImage = `url(${evt.target.src})`
+}
+
+travelImg.addEventListener("click",picSelect)
+
+
+let houseImg1= document.getElementById('house')
+let picSelect12 = (evt) => {
+    let item2 =document.getElementById('item2')
+    item2.style.backgroundImage = `url(${evt.target.src})`
+}
+
+houseImg.addEventListener("click",picSelect)
+
+
+
+let familyImg1= document.getElementById('family')
+let picSelect13 = (evt) => {
+    let item2 =document.getElementById('item2')
+    item2.style.backgroundImage = `url(${evt.target.src})`
+}
+
+familyImg.addEventListener("click",picSelect)
+
+
+let relaxImg1= document.getElementById('relax')
+let picSelect14 = (evt) => {
+    let item2 =document.getElementById('item2')
+    item2.style.backgroundImage = `url(${evt.target.src})`
+}
+
+relaxImg.addEventListener("click",picSelect)
+
+let carImg1= document.getElementById('car')
+let picSelect15 = (evt) => {
+    let item2 =document.getElementById('item2')
+    item2.style.backgroundImage = `url(${evt.target.src})`
+}
+
+carImg.addEventListener("click",picSelect)
